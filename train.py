02de8dcf41b73e10a2cec0f460e23f6360d6b6f4
WBennett003 <109073730+WBennett003@users.noreply.github.com>
2023-01-08 13:14:40 +0000

    yes

--- train.py
+++ train.py
@@ -3,12 +3,14 @@ import numpy as np
 import h5py
 import math
 import matplotlib.pyplot as plt
+import time
+import wandb
 
 from transformer_model import Transformer
 #simple proof of concept model
 
 class dataset(torch.utils.data.Dataset):
-  def __init__(self, file_path='datasets/testdataset.h5py'):
+  def __init__(self, file_path='datasets/dense_bonding10page.h5py'):
     super().__init__()
     self.file = h5py.File(file_path, 'r+')
 
@@ -17,31 +19,49 @@ class dataset(torch.utils.data.Dataset):
   
   def __getitem__(self, idx):
     return (self.file['reactant_elem'][idx], self.file['reactant_charge'][idx], self.file['reactant_pos'][idx],
-    self.file['reactant_bond_idx'][idx], self.file['reactant_bond_type'][idx], 
+    self.file['reactant_adj'][idx], 
     self.file['product_elem'][idx], self.file['product_charge'][idx], self.file['product_pos'][idx],
-    self.file['product_bond_idx'][idx], self.file['product_bond_type'][idx], 
-    self.file['theozyme_elem'][idx], self.file['theozyme_pos'][idx], self.file['theozyme_bond_idx'][idx], self.file['theozyme_bond_type'][idx]
-    )
+    self.file['product_adj'][idx], 
+    self.file['theozyme_elem'][idx], self.file['theozyme_pos'][idx], self.file['theozyme_adj'][idx])
 
 def get_target(TE, TP, TBI, max_length=1000):
   TE = TE.roll(-1, 1)
   TP = TP.roll(-1, 1)
+  TBI = TBI.roll(-1, 1)
   #so the last element is not the first target
-  TBI += 1 #as i have rolled each element down one
 
   return TE, TP, TBI
 
 THEOZYME_SIZE = 1000
-COMPOUND_SIZE = 500
-ELEMENT_SIZE = 20
+COMPOUND_SIZE = 1000
+ELEMENT_SIZE = 30
 BOND_SIZE = 8
 
-NHEADS = 2
-NBLOCKS = 2
-DMODEL  = 20 #embedding size
-DFF = 255 #high dim upscale in fead forward layer
+NHEADS = 1
+NBLOCKS = 1
+DMODEL  = 5 #embedding size
+DFF = 10 #high dim upscale in fead forward layer
 
-BATCH_SIZE = 4
+BATCH_SIZE = 1
+LEARNING_RATE = 0.02
+EPOCHS = 5
+
+wandb.init(
+    # set the wandb project where this run will be logged
+    project="Theozmye-Transformer",
+    
+    # track hyperparameters and run metadata
+    config={
+    "learning_rate": LEARNING_RATE,
+    "n_heads": NHEADS,
+    "n_blocks": NBLOCKS,
+    "DMODEL": DMODEL,
+    "DFF": DFF,
+    "batch_size": BATCH_SIZE,
+    "dataset": "10page_dense",
+    "epochs": EPOCHS,
+    }
+)
 
 data = dataset()
 loader = torch.utils.data.DataLoader(data, batch_size=BATCH_SIZE, shuffle=True)
@@ -51,40 +71,58 @@ model = Transformer(nheads=NHEADS, nblocks=NBLOCKS, dff=DFF, dmodel=DMODEL, elem
 loss_func_elemnt = torch.nn.CrossEntropyLoss()
 loss_func_bonding = torch.nn.CrossEntropyLoss()
 loss_func_pos = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters())
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-epochs = 100
+epochs = EPOCHS
+batch_length = len(loader)
+
 
 outputs = []
 losses = []
+avg_loss = []
+
 
 for epoch in range(epochs):
-  for RE, RC, RP, RBI, RBT, PE, PC, PP, PBI, PBT, TE, TP, TBI, TBT in loader:
-    tgtE, tgtP, tgtBI = get_target(TE, TP, TBI, THEOZYME_SIZE)
-    pred_elem, pred_pos, pred_bonding = model(RE, RC, RP, RBI, RBT, PE, PC, PP, PBI, PBT, tgtE, tgtP, tgtBI, TBT)
+  count = 0
+  for RE, RC, RP, RADJ, PE, PC, PP, PADJ, TE, TP, TADJ in loader:
+    start = time.time()
+    tgtE, tgtP, tgtADJ = get_target(TE, TP, TADJ, THEOZYME_SIZE)
 
-    bonding = torch.sparse_coo_tensor(TBI, TBT, pred_bonding.shape)
-    bonding = bonding.to_dense()
+    pred_elem, pred_pos, pred_bonding = model(RE, RC, RP, RADJ, PE, PC, PP, PADJ, tgtE, tgtP, tgtADJ)
 
-    zeros = torch.zeros((TE.shape[0], TE.shape[1], pred_elem.shape[-1]))
-    mask = torch.arange(pred_elem.shape[-1])[None, None, :] == TE[:, :, None]
-    zeros[mask] = 1
-    TE = zeros
-
-    loss_elem = loss_func_elemnt(pred_elem, TE)
+    loss_elem = loss_func_elemnt(pred_elem, torch.nn.functional.one_hot(TE.long(), ELEMENT_SIZE).float())
     loss_pos = loss_func_pos(pred_pos, TP)
-    loss_bonding = loss_func_pos(pred_bonding, bonding)
+    loss_bonding = loss_func_pos(pred_bonding, torch.nn.functional.one_hot(TADJ.long(), BOND_SIZE).float())
     loss = loss_elem + loss_bonding + loss_pos
-
 
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
     losses.append([loss_elem.detach().numpy(), loss_pos.detach().numpy(), loss_bonding.detach().numpy()])
+    print(f"{count}/{batch_length} : loss {loss.detach().numpy()} : {round(time.time()-start, 2)}s")
+    count += 1
+
   print(f"epoch {epoch} : loss {loss_elem, loss_pos, loss_bonding}")
   torch.save(model.state_dict(), 'weights/transformer.pt')
+  start = batch_length * epoch
+  end = batch_length * (epoch+1)
+  temp_losses = np.array(losses[start:end])
+  avg_loss.append([
+      temp_losses[0].mean(),
+      temp_losses[1].mean(),
+      temp_losses[2].mean(),
+      temp_losses[3].mean(),
+  ])
 
+  wandb.log({
+      "epoch" : epoch,
+      "loss" : avg_loss[epoch][0],
+      "loss-elem" : avg_loss[epoch][1],
+      "loss-pos" : avg_loss[epoch][2],
+      "loss-pos" : avg_loss[epoch][3],
+      "weights" : model.state_dict()
+  })
 n = 10
 losses = np.array(losses).T
 plt.plot(np.arange(n), losses[0, -n:, ])
